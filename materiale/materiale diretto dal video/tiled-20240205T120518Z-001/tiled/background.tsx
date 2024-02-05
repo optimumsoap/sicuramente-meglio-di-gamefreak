6baaa8850623fa8b1ffff1c97edd5b1e1ee16d83
optimumsoap <rikyzani2006@gmail.com>
2024-02-05 13:07:52 +0100

    aggiunta nella cartella materiali le immagini e gli sprite presi direttamente dal video

---
+++ materiale/materiale diretto dal video/tiled-20240205T120518Z-001/tiled/background.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="background" tilewidth="288" tileheight="180" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="288" height="180" source="../stringstar fields/background_0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="288" height="180" source="../stringstar fields/background_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="288" height="180" source="../stringstar fields/background_2.png"/>
+ </tile>
+</tileset>
---
+++ materiale/materiale diretto dal video/tiled-20240205T120518Z-001/tiled/springstar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="springstar" tilewidth="16" tileheight="16" tilecount="198" columns="18">
+ <image source="../stringstar fields/tileset.png" width="288" height="176"/>
+</tileset>
