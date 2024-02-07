28a8f0136f940508728b07fc2f71f4092da98ebe
optimumsoap <rikyzani2006@gmail.com>
2024-02-07 23:33:13 +0100

    arrivato fino al minuto 42:50 del video, concluso gravità e aggiunto meccaniche di movimento del personaggio, purtroppo sono ancora presenti degli errori la quale riportano, che alcune definizioni di funzioni siano errate nonostante tramite un controllo siano state appurate sintaticcamente correte.

--- codice/index.js
+++ codice/index.js
@@ -6,6 +6,7 @@ canvas.width = 1024 //(canvas.width e .height) ridimensionamento del nostro canv
 canvas.height = 576
 //(c.fillRect) viene utilizzato per disegnare un rettangolo pieno. Con quattro parametri: le coordinate x e y e la larghezza e l'altezza del rettangolo da disegnare.
 
+const gravity = 0.5
 
 
 // CREAZIONE GRAVITA'
@@ -15,19 +16,28 @@ canvas.height = 576
 class Player { //Le classi sono un modello per creare oggetti.
     constructor(position){ //permette di identificare tutte le proprietà del nostro player
         this.position = position 
-        
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.height = 100
     }
 
     draw() {
         c.fillStyle = 'red' //colore dell'hitbox player
-        c.fillRect = (this.position.x , this.position.y, 100, 100) //dimensione del player
+        c.fillRect = (this.position.x , this.position.y, 100, this.height) //dimensione del player
         //nota n2= utilizziamo invece di inserire le coordinate manualmente, le coordinate definite nel constructor precedente con "this.position.x" e .y
     }
 
     //adesso però dobbiamo fargli cambiare le coordinate
     update(){
         this.draw() //(con .draw il nostro bel player comparirà)
-        this.position.y++ // come in c++ i due + servono a causare un incremento singolo
+
+        this.position.y += this.velocity.x //creazione di una variabile velocità così da permettere di cambiare più agilmente la velocità rispetto ad un semplice y++
+        this.position.y += this.velocity.y
+        if(this.position.y + this.height + this.velocity.y < canvas.height) //funzione realistica di gravità aumentando la velocità sempre più mentre si cade
+        this.velocity.y += gravity //valore della gravità
+        else this.velocity.y = 0 //blocco della caduta quando si tocca la fine della pagina
     }
 }
 
@@ -40,16 +50,51 @@ const player2 = new Player({
     y: 100,
 })
 
-
+const keys = { //tutti i tasti della tastiera che voglio "sentire"
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+}
 
 function animazione(){
     window.requestAnimationFrame(animazione)  //Il metodo window.requestAnimationFrame() dice al browser che desideri eseguire un'animazione.
     c.fillStyle = 'white' //il colore del canvas è di base nero
-    c.fillRect(0, 0, canva.width, canva.height) 
+    c.fillRect(0, 0, canvas.width, canvas.height) 
     player.update() //abbiamo inserito il nostro player che cade all'interno dell'animazione sopra scritta (update ne aggiorna la posizione facendolo cadere verso il basso)
     player2.update() // le sue coordinate vanno aggiornate altrimenti sarebbero sovrapposti visto che utilizziamo le stesse proprietà che possiede anche il player1
+
+    player.velocity.x = 0
+    if(keys.d.pressed)player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
 
 } 
 
 
 animazione() //quando questa funzione viene chiamata nel codice permette alla funzione di andare in loop grazie al comando "window.requestAnimationframe" che rimanda alla funzione "animazione" grazie al fatto che contiene essa stessa all'interno delle parentesi.
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+    case 'd':
+    keys.d.pressed = true //movimento a destra
+    breal
+    case 'a':
+    keys.a.pressed = true //movimento a sinistra
+    break
+    case 'w':
+    player.velocity.y = -20 //salto
+    break
+    }
+})
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+    case 'd':
+    keys.d.pressed = false //movimento a destra
+    breal
+    case 'a':
+    keys.a.pressed = false //movimento a sinistra
+    break
+    }
+})
