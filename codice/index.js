8a89e64cf6ea292819359daf75b9e40ed8565201
optimumsoap <rikyzani2006@gmail.com>
2024-02-08 11:57:37 +0100

    risoluzione del bug del movimento

--- codice/index.js
+++ codice/index.js
@@ -18,14 +18,14 @@ class Player { //Le classi sono un modello per creare oggetti.
         this.position = position 
         this.velocity = {
             x: 0,
-            y: 1
+            y: 1,
         }
         this.height = 100
     }
 
     draw() {
         c.fillStyle = 'red' //colore dell'hitbox player
-        c.fillRect = (this.position.x , this.position.y, 100, this.height) //dimensione del player
+        c.fillRect(this.position.x , this.position.y, 100, this.height) //dimensione del player
         //nota n2= utilizziamo invece di inserire le coordinate manualmente, le coordinate definite nel constructor precedente con "this.position.x" e .y
     }
 
@@ -33,7 +33,7 @@ class Player { //Le classi sono un modello per creare oggetti.
     update(){
         this.draw() //(con .draw il nostro bel player comparirà)
 
-        this.position.y += this.velocity.x //creazione di una variabile velocità così da permettere di cambiare più agilmente la velocità rispetto ad un semplice y++
+        this.position.x += this.velocity.x //creazione di una variabile velocità così da permettere di cambiare più agilmente la velocità rispetto ad un semplice y++
         this.position.y += this.velocity.y
         if(this.position.y + this.height + this.velocity.y < canvas.height) //funzione realistica di gravità aumentando la velocità sempre più mentre si cade
         this.velocity.y += gravity //valore della gravità
@@ -73,18 +73,17 @@ function animazione(){
 } 
 
 
-animazione() //quando questa funzione viene chiamata nel codice permette alla funzione di andare in loop grazie al comando "window.requestAnimationframe" che rimanda alla funzione "animazione" grazie al fatto che contiene essa stessa all'interno delle parentesi.
 
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
     case 'd':
     keys.d.pressed = true //movimento a destra
-    breal
+    break
     case 'a':
     keys.a.pressed = true //movimento a sinistra
     break
     case 'w':
-    player.velocity.y = -20 //salto
+    player.velocity.y = -15 //salto
     break
     }
 })
@@ -92,9 +91,11 @@ window.addEventListener('keyup', (event) => {
     switch (event.key) {
     case 'd':
     keys.d.pressed = false //movimento a destra
-    breal
+    break
     case 'a':
     keys.a.pressed = false //movimento a sinistra
     break
     }
 })
+
+animazione() //quando questa funzione viene chiamata nel codice permette alla funzione di andare in loop grazie al comando "window.requestAnimationframe" che rimanda alla funzione "animazione" grazie al fatto che contiene essa stessa all'interno delle parentesi.
